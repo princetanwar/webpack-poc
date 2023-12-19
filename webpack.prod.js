e15194880a8d8529837db8ad390aadd9500fbdbd
prince tanwar <prince.t@simformsolutions.com>
2023-12-19 18:44:15 +0530

    add css minification support

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,10 @@
 const { merge } = require("webpack-merge");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin')
+
 const commonConfig = require("./webpack.common.js");
+
 
 module.exports = merge(commonConfig, {
   mode: "production",
@@ -11,6 +15,12 @@ module.exports = merge(commonConfig, {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
+    ],
+  },
+  optimization: {
+    minimizer: [
+   new CssMinimizerPlugin(),
+      new TerserPlugin(),
     ],
   },
 });
